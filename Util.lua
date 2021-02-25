ddedd8ea51cdbe1e2a0587583ce60ceb7fbb6be9
Sam Mousa <sam@mousa.nl>
2021-02-25 14:54:40 +0100

    update toc

---
+++ Util.lua
@@ -0,0 +1,206 @@
+
+if Util == nil then
+    Util = {}
+end
+-- Searches for the first index that has value >= to the given value
+function Util.BinarySearch(list, value, comparator, min, max)
+    if type(list) ~= 'table' then
+        error("Argument 1 must be a table")
+    end
+    local result
+    if min == nil then
+        min = 1
+        max = #list
+    end
+
+    if min > max then
+        return nil
+    end
+
+    local test = math.floor((max + min) / 2)
+
+    if comparator == nil then
+        result = list[test] < value
+    else
+        result = comparator(list[test], value)
+    end
+
+
+    if result then
+        -- Tested element < value, solution not in left side
+        return Util.BinarySearch(list, value, comparator, test + 1, max)
+    elseif test == min then
+        return test
+    else
+        -- Tested element >= value, solution is in left side or current element
+        return Util.BinarySearch(list, value, comparator, min, test)
+    end
+end
+
+function Util.IsSorted(list, comparator)
+    if comparator == nil then
+        comparator = function(a, b)
+            return a < b
+        end
+    end
+
+    for i = 1, #list - 1  do
+        if not comparator(list[i], list[i+1]) then
+            return false
+        end
+    end
+    return true
+end
+
+function Util.DumpTable(table, indent)
+
+    indent = indent or 0;
+
+    local keys = {};
+
+    for k in pairs(table) do
+        keys[#keys+1] = k;
+    end
+
+    print(string.rep('  ', indent)..'{');
+    indent = indent + 1;
+    for k, v in pairs(table) do
+
+        local key = k;
+        if (type(key) == 'string') then
+            if not (string.match(key, '^[A-Za-z_][0-9A-Za-z_]*$')) then
+                key = "['"..key.."']";
+            end
+        elseif (type(key) == 'number') then
+            key = "["..key.."]";
+        end
+
+        if (type(v) == 'table') then
+            if (next(v)) then
+                print(string.rep('  ', indent), tostring(key), "=");
+                Util.DumpTable(v, indent);
+            else
+                print(string.rep('  ', indent), tostring(key), "= {},");
+            end
+        elseif (type(v) == 'string') then
+            print(string.rep('  ', indent), tostring(key), " = ", "'"..v.."'");
+        else
+            print(string.rep('  ', indent), tostring(key), " = ", tostring(v));
+        end
+    end
+    indent = indent - 1;
+    print(string.rep('  ', indent)..'}');
+end
+
+function Util.CreateFieldSorter(field)
+    return function(a, b)
+        return a[field] < b[field]
+    end
+end
+
+-- Return the 1-based unix epoch week number
+function Util.WeekNumber(unixtimestamp)
+    return 1 + math.floor(unixtimestamp / 604800)
+end
+
+function Util.CreateMultiFieldSorter(field1, field2)
+    return function(a, b)
+        if a[field1] == b[field1] then
+            return a[field2] < b[field2]
+        else
+            return a[field1] < b[field1]
+        end
+    end
+end
+
+
+function Util.InvertSorter(sorter)
+    return function(a, b)
+        return not sorter(a, b)
+    end
+end
+
+function Util.time()
+    if GetServerTime then
+        return GetServerTime()
+    else
+        return os.time()
+    end
+end
+
+function Util.UUID()
+    local random = math.random(1, 2^31 - 1)
+    local ts = Util.time()
+
+    local hex = string.format('%08x%08x', ts, random)
+    return hex
+end
+
+
+
+
+function Util.TestBinarySearch()
+    local cases = {
+        { list = { 1, 2}, search = 2, expected = 2 },
+        { list = { 1, 2, 3}, search = 2, expected = 2 },
+        { list = { 1, 2, 3, 4, 5, 6}, search = 14, expected = nil },
+        { list = { 1, 2, 3, 4, 5, 6}, search = 2, expected = 2 },
+        { list = { 10, 20, 30, 40, 50, 60}, search = 20, expected = 2 },
+        { list = { 1, 2, 4, 5, 6}, search = 4, expected = 3 },
+        { list = { 1, 2, 3, 5, 6}, search = 4, expected = 4 }
+
+    }
+
+    for _, v in ipairs(cases) do
+        local result = Util.BinarySearch(v.list, v.search)
+        if result ~= v.expected then
+            print("Test FAIL: [", table.concat(v.list, ', '), "], search: ", v.search, " expected: ", v.expected, " got: ", result)
+        else
+            print("Test PASS: [", table.concat(v.list, ', '), "], search: ", v.search, " expected: ", v.expected, " got: ", result)
+        end
+
+    end
+
+end
+
+function Util.IntegerToBytes(number)
+    return math.floor(number / 2^24),
+        math.floor((number % 2^24) / 2^16),
+        math.floor((number % 2^16) / 2^8),
+        number % 2^8
+
+end
+
+function Util.ByteSum(number)
+    return math.floor(number / 2^24)
+            + math.floor((number % 2^24) / 2^16) + math.floor((number % 2^16) / 2^8) + number % 2^8;
+end
+function Util.IntegerChecksumCoroutine()
+    -- for i=1, #str do
+    -- 		local s = string.byte(str, i, i)
+    -- 		a = (a+s)%65521
+    -- 		b = (b+a)%65521
+    -- 		end
+    -- return b*65536+a
+    return coroutine.create(function(number)
+        local a = 1
+        local b = 0
+        local b1, b2, b3, b4
+
+        local next = number
+        while(true) do
+            b1, b2, b3, b4 = Util.IntegerToBytes(next)
+            a = (a + b1)
+            b = (b + a)
+            a = (a + b2)
+            b = (b + a)
+            a = (a + b3)
+            b = (b + a)
+
+            a = (a + b4) % 65521
+            b = (b + a) % 65521
+
+            next = coroutine.yield(b * 65536 + a)
+        end
+    end)
+end
