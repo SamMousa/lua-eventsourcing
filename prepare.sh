21c006eb6d1dac6e0ea6dfa26b9e837866a6570d
Sam Mousa <sam@mousa.nl>
2021-10-24 23:03:57 +0200

    ci: move build to semantic-release plugin

--- prepare.sh
+++ prepare.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
-echo $@
-exit 1 # prevent release for now
+sed -i "s/{VERSION}/$1/g" LibEventSourcing.toc
+# add deps
+gitman install
+# Remove git dirs
+find libs -type d -name ".git" -execdir rm -rf {} \; -prune
+mkdir LibEventSourcing
+mv readme.MD LibEventSourcing/
+mv source LibEventSourcing/
+mv libs LibEventSourcing/
+zip -r LibEventSourcing.zip LibEventSourcing
